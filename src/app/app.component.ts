342285de09b8375a784957e72046dc0ef68a7bdc
yilberth <yilberth@mavecure.co>
2023-05-12 04:16:03 -0500

    final upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { FormBuilder, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['../sass/styles.scss']
 })
-export class AppComponent {
-  title = 'my-test';
+  
+export class AppComponent  {
+  userform = this.fb.group({
+    sex: ['', Validators.required],
+    date_birthday: ['', Validators.required],
+    name: ['', Validators.required],
+    last_name: ['', Validators.required],
+    email: ['', Validators.required],
+    address: ['', Validators.required],
+    country: ['', Validators.required],
+    Department: ['', Validators.required],
+    city: ['', Validators.required],
+    comment: ['', Validators.required],
+  })
+  constructor(private fb: FormBuilder) { }
+  onSubmit() {
+  alert(JSON.stringify(this.userform.value));
+  }
 }
